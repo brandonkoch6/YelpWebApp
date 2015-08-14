7b5ad231f49d49507136b09678271aa331194ba2
Brandon Koch <brandon.gregory.koch@gmail.com>
2015-08-13 22:29:03 -0700

    Created template view

--- app/core/views.py
+++ app/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 # Create your views here.
-def TestView(request, **kwargs):
-	return HttpResponse("Hello World!")
+class SplashView(TemplateView):
+	template_name = "index.html"
